4d6f43b22845f2e2c3c7c0b284599b2ecb22e3c1
Petrica Nanca <petrica_nanca@yahoo.com>
2024-09-20 11:06:09 +0300

    Add welcome

--- src/components/admin-setup/admin-setup.tsx
+++ src/components/admin-setup/admin-setup.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from "./admin-setup.module.scss";
 
 export const AdminSetup = () => {
-    return (
-     <div>Admin setup</div>
-    )
-}
+  return (
+    <div className={styles["welcome-wrapper"]}>
+      <div className={styles["logo"]}>
+        <Image
+          src="/logo-main-small.png"
+          alt="Picture Pulse Logo"
+          width={84}
+          height={84}
+          priority
+        />
+      </div>
+      <div className={styles["message-box"]}>
+        <div className={styles["welcome"]}>Welcome</div>
+      </div>
+    </div>
+  );
+};
 
 export default AdminSetup;
-
-
