387e58d3380e525d4abadabafac1fd19083dec0c
Petrica Nanca <petrica_nanca@yahoo.com>
2024-11-18 15:54:50 +0200

    Add the admin user into the database on install

--- src/app/api/setup/route.ts
+++ src/app/api/setup/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
+import mysql from 'mysql2/promise';
 import { promises as fs } from 'fs';
 import path from 'path';
+import bcrypt from 'bcrypt';
 
 const dbFilePath = path.join(process.cwd(), 'config', 'database.json');
 
@@ -14,6 +16,44 @@ export async function POST(req: Request) {
     }
     dbData.adminSet = true;
     await fs.writeFile(dbFilePath, JSON.stringify(dbData, null, 2));
+    
+    const body = await req.json();
+    const { sitePassword, siteUsername } = body;
+    const hashedPassword = await bcrypt.hash(sitePassword, 10);
+
+    const connectionConfig = {
+      host: dbData.dbHost || '127.0.0.1',
+      port: dbData.dbPort, 
+      database: dbData.dbName,
+      user: dbData.dbUser,
+      password: dbData.dbPassword,
+    };
+    let connection;
+    connection = await mysql.createConnection(connectionConfig);
+
+    // Create the users table if it does not exist
+    await connection.execute(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role ENUM('ADMIN', 'USER') DEFAULT 'USER',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `);
+
+    // Insert the admin user into the table if it doesn't exist
+    const [rows] = await connection.execute(
+      `SELECT * FROM users WHERE username = ?`,
+      [siteUsername]
+    );
+
+    if ((rows as any[]).length === 0) {
+      await connection.execute(
+        `INSERT INTO users (username, password, role) VALUES (?, ?, 'ADMIN')`,
+        [siteUsername, hashedPassword]
+      );
+    }
 
     return NextResponse.json({ message: 'Admin setup completed successfully!' , status: 200 });
 
